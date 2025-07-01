d174538e959f0d7f7a5cca62affa8b94c575a2e9
NyLaurent <laulanyumbayire@gmail.com>
2025-06-30 19:09:48 -0500

    final commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import ContactHero from "@/components/ContactHero";
+import LocationMap from "@/components/LocationMap";
+import UpcomingEvents from "@/components/UpcomingEvents";
+
+export default function Contact() {
+  return (
+    <main>
+      <ContactHero />
+      <LocationMap />
+      <UpcomingEvents />
+    </main>
+  );
+}
