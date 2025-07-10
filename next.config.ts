d8f54080bf86c383df0e67830861906138e9c20e
NyLaurent <laulanyumbayire@gmail.com>
2025-07-09 19:26:18 -0500

    build errors solved

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['react-leaflet', 'react-loader-spinner'],
+  experimental: {
+    esmExternals: 'loose'
+  }
 };
 
 export default nextConfig;
