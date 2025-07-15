995ff7e50e2bcedb0fb7bf3f2e2a722f467510c2
NyLaurent <laulanyumbayire@gmail.com>
2025-07-15 17:59:37 -0500

    fix: build error

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-white border-b border-white/20 fixed w-full top-0 z-50">
+      <nav className="bg-white border-b border-white/20 fixed w-full top-0 z-50 lg:bg-white/60 lg:backdrop-blur-md">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16 sm:h-20">
             {/* Logo and Company Name */}
